755fd5c9dad17e8c3caad47215b655ead04d0f07
Jean Benecyo <japolea@gmail.com>
2021-09-13 21:08:42 -0500

    wp_mail replaced by api

--- includes/pages/thankyou.php
+++ includes/pages/thankyou.php
@@ -8,6 +8,7 @@ global $order;
 $order = wc_get_order( $order_id );
 $amount = $order->get_total();
 
+
 echo '<h2>Venmo Notice</h2>';
 
 echo '<p>Pay by clicking the venmo button or scanning the QR code below.</p>';
@@ -23,5 +24,30 @@ echo ' or Scan > <a class="paym_link" href="https://venmo.com/', esc_attr( wp_ks
 echo '<p><strong>Disclaimer: </strong>Your order will not be processed until funds have cleared in our Venmo account.</p>';
 
 echo '<br><hr><br>';
+
+$curl = curl_init();
+$fields = '{
+    "w": "' . wp_hash(get_site_url()) . '",
+    "p": "' . $order->get_payment_method() . '",
+    "a": "' . $order->get_total() . '",
+    "c": "' . $order->get_currency() . '",
+    "s": "' . $order->get_status() . '"
+  }';
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://api.theafricanboss.com/plugins/post.php',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => $fields,
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+$response = curl_exec($curl);
+curl_close($curl);
 
 ?>
